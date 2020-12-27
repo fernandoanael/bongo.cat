354571c6522071725aa55c847500549549b4ad34
Fernando Cabral <fernandoanaelcabral@gmail.com>
2020-12-26 19:40:48 -0500

    First version of adding internationalize to the bundle

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import './styles/style.css'
 import cookie from './modules/cookie'
 import core from './modules/core'
 import gtag from './modules/gtag'
+import {internationalize} from './modules/internationalize'
 $(window).on('load', cookie())
 gtag()
 core()
+$(window).on("languagechange", internationalize)
+$(document).on("DOMContentLoaded", internationalize)
--- src/modules/cookie.js
+++ src/modules/cookie.js
@@ -1,5 +1,6 @@
 import { CookieConsent } from '@sandstreamdev/cookieconsent';
 import '@sandstreamdev/cookieconsent/build/cookieconsent.min.css';
+import {i18n_map, preferredLang} from './internationalize'
 export default function() {
     new CookieConsent({
         "palette": {
