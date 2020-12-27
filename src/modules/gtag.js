a8865a0cfaf013e2a1fe7f818453703fb3477082
Fernando Cabral <fernandoanaelcabral@gmail.com>
2020-12-27 16:59:17 -0500

    Fix Gtag issue

--- src/modules/gtag.js
+++ src/modules/gtag.js
@@ -4,11 +4,10 @@ import googleTagManager from '@analytics/google-tag-manager'
 export default function() {
     const analytics = new Analytics({
         app: 'BongoCat',
-        plugins: {
-            googleTagManager: {
-                containerID: 'UA-120277085-2',
-                js: new Date()
-            }
-        }
+        plugins: [
+            googleTagManager({
+                containerId: 'UA-120277085-2'
+            })
+        ]
     })
 }
